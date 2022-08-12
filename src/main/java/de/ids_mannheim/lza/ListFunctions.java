7c6b7144024b17411fb4f2568c8823749c530724
Herbert Lange <lange@ids-mannheim.de>
2022-08-12 12:29:43 +0200

    fix listing of functions by using springboot to list beans instead of reflections

--- src/main/java/de/ids_mannheim/lza/ListFunctions.java
+++ src/main/java/de/ids_mannheim/lza/ListFunctions.java
@@ -1,19 +1,22 @@
 package de.ids_mannheim.lza;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.reflections.Reflections;
-import java.lang.reflect.*;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @RestController
 public class ListFunctions {
-    class FunctionInfo {
+
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
+    public class FunctionInfo {
         String name;
         String description;
         Map<String,String> params;
@@ -53,17 +56,24 @@ public class ListFunctions {
      * @return list of information about all functions
      */
     @GetMapping("/")
-    public List<FunctionInfo> listFunctions() throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+    public List<FunctionInfo> listFunctions() {
         // Get all functions using reflections
         List<FunctionInfo> info = new ArrayList<>();
-        Reflections reflections = new Reflections(this.getClass().getPackageName());
-        // Get all classes derived from Function
-        for (Class<? extends Function> f :
-                 reflections.getSubTypesOf(Function.class).stream()
-                        .filter((c) -> !Modifier.isAbstract(c.getModifiers())).collect(Collectors.toSet())) {
-            Function fo = f.getConstructor().newInstance();
-            info.add(new FunctionInfo(fo.getName(), fo.getDescription(), fo.getParameters()));
+        for (Map.Entry<String,Function> fun : applicationContext.getBeansOfType(Function.class).entrySet()) {
+            info.add(new FunctionInfo(fun.getValue().getName(),
+                    fun.getValue().getDescription(),
+                    fun.getValue().getParameters())
+            );
         }
+//        Reflections reflections = new Reflections(this.getClass().getPackageName());
+//        // Get all classes derived from Function
+//        for (Class<? extends Function> f :
+//                 reflections.getSubTypesOf(Function.class).stream()
+//                        .filter((c) -> !Modifier.isAbstract(c.getModifiers())).collect(Collectors.toSet())) {
+//            Function fo = f.getConstructor().newInstance();
+//            info.add(new FunctionInfo(fo.getName(), fo.getDescription(), fo.getParameters()));
+//        }
+        Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Info{0}", info.stream().map((i) -> i.name).collect(Collectors.joining(",")));
 	return info;
     }
 }
