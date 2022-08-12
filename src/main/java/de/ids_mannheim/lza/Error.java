7dcf2f3a65273b7148db7b6e6b04846fbcaf88e2
Herbert Lange <lange@ids-mannheim.de>
2022-08-12 12:30:38 +0200

    improve error listing

--- src/main/java/de/ids_mannheim/lza/Error.java
+++ src/main/java/de/ids_mannheim/lza/Error.java
@@ -6,10 +6,41 @@ import org.springframework.web.bind.annotation.*;
 
 @ControllerAdvice
 public class Error {
+    class ErrorInfo {
+        String desription;
+        String exception;
+        String cause;
+
+        public ErrorInfo(Exception exception) {
+            this.desription = exception.getMessage();
+            this.exception = exception.getClass().getName();
+            if (exception.getCause() != null)
+                this.cause = exception.getCause().toString();
+        }
+
+        @Override
+        public String toString() {
+            return "ErrorInfo{" + "desription=" + desription + ", exception=" + exception + ", cause=" + cause + '}';
+        }
+
+        public String getDesription() {
+            return desription;
+        }
+
+        public String getException() {
+            return exception;
+        }
+
+        public String getCause() {
+            return cause;
+        }
+
+    }
 
     @ResponseBody
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(Exception exception) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.toString());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorInfo(exception));
     }
 }
